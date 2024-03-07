a75d75ba81966fc4067e1cb7b1425302a60714b8
Santhosh <pitakasanthosh@gmail.com>
2024-03-07 22:51:01 +0530

    Add Header component with navigation links

---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const navigationListItems = [
+  { label: 'Home', path: '/' },
+  { label: 'Projects', path: '/#projects' },
+  { label: 'Contact', path: '/#contact' },
+];
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="bg-gray-50 shadow-md fixed w-full z-10">
+      <div className="container mx-auto px-6 py-3 md:flex md:items-center md:justify-around">
+        <div className="flex items-center justify-between">
+          <div className="text-3xl font-bold text-gray-900" style={{fontFamily:"Montserrat"}}>
+            <Link to="/" className="hover:text-blue-400 transition duration-200">
+              Santhosh Mani 
+            </Link>
+          </div>
+          <button
+            className="lg:hidden block"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? (
+              <span className="material-icons hover:text-blue-500">close</span>
+            ) : (
+              <span className="material-icons hover:text-blue-500">menu</span>
+            )}
+          </button>
+        </div>
+        <nav className={`lg:flex transition-all duration-500 ease-in-out ${isOpen ? 'block' : 'hidden'} mt-4 lg:mt-0`}>
+          <ul className="flex flex-col lg:flex-row items-center justify-center lg:space-x-10 space-y-2 lg:space-y-0">
+            {navigationListItems.map((item, index) => (
+              <li key={index}>
+                <Link
+                  to={item.path}
+                  className="hover:underline transition duration-200 font-sans text-lg text-left hover:text-blue-600" style={{fontFamily: 'Montserrat'}}
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
