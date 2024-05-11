12d5c2422e588da89361e851f3fcf6dfce609f57
santhosh <pitakasanthosh@gmail.com>
2024-05-11 07:21:24 +0530

    Fix : Made social links clickable on label as well

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,24 +28,25 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div className="flex flex-col items-center py-3 hover:text-white text-slate-100  bg-slate-950  footer-wrapper justify-evenly md:flex-row sm:font-mono lg:font-sans">
-          <div className="copyright-info lg:text-2xl text-lg">
+        <div className="flex flex-col items-center py-3 font-mono hover:text-white text-slate-100 bg-slate-950 footer-wrapper justify-evenly md:flex-row">
+          <div className="text-lg copyright-info lg:text-xl">
             &copy; Santhosh Mani  {new Date().getFullYear()}
           </div>
           <div className="grid grid-cols-2 grid-rows-2">
             {socials.map((social, index) => {
               return (
-                <div key={index} className="flex items-center justify-evenly px-8 py-2 m-2 mx-2 border-2 border-blue-900 hover:border-blue-500 rounded-xl text-gray-400 hover:text-slate-50 hover:cursor-pointer ">
-                  <span>{social.name}</span>
+                <div key={index} className="flex items-center p-2 m-2 mx-2 text-gray-400 duration-300 border-2 border-blue-900 p2 justify-evenly hover:border-blue-500 rounded-xl hover:text-slate-50 hover:cursor-pointer hover:bg-slate-950 focus:border-blue-800 hover:shadow-sm hover:shadow-green-400">
                   <a
                     key={index}
                     href={social.url}
                     target="_blank"
                     rel="noreferrer"
                     aria-label={social.name}
-                    className="mx-4 text-3xl transition duration-200"
+                    className="flex items-center text-lg transition duration-200 justify-evenly lg:text-2xl"
                   >
-                    {social.icon}
+                    <span className="px-2">{social.name}</span>
+                    <span className="px-2">{social.icon}</span>
+                    <span className="text-sm font-semibold text-blue-800 material-icons animate-pulse">arrow_outward</span>
                   </a>
                 </div>
               );
