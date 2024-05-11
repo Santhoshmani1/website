bf6c66609e8e8e8350ba45ffeaa66ec9184a5e0e
santhosh <pitakasanthosh@gmail.com>
2024-05-11 07:17:50 +0530

    Feat: Improve social links appearance in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,21 @@
 import { FaLinkedinIn, FaGithub, } from "react-icons/fa";
-import { SiX } from "react-icons/si";
+import { SiGmail, SiX } from "react-icons/si";
 
 const socials = [
   {
-    name: "LinkedIn",
-    url: "https://www.linkedin.com/in/santhoshmanip",
-    icon: <FaLinkedinIn />,
+    name: "Email",
+    url: "mailto:pitakasanthosh@gmail.com",
+    icon: <SiGmail />
   },
   {
     name: "GitHub",
     url: "https://github.com/santhoshmani1",
     icon: <FaGithub />,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/santhoshmanip",
+    icon: <FaLinkedinIn />,
   },
   {
     name: "Twitter",
@@ -23,23 +28,26 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div className="footer-wrapper bg-black text-white p-5 flex justify-evenly items-center md:flex-row  flex-row" style={{ fontFamily: "Montserrat" }}>
-          <div className="copyright-info">
-              &copy; Santhosh Mani  {new Date().getFullYear()}
+        <div className="flex flex-col items-center py-3 hover:text-white text-slate-100  bg-slate-950  footer-wrapper justify-evenly md:flex-row sm:font-mono lg:font-sans">
+          <div className="copyright-info lg:text-2xl text-lg">
+            &copy; Santhosh Mani  {new Date().getFullYear()}
           </div>
-          <div className="flex justify-center items-center py-2">
+          <div className="grid grid-cols-2 grid-rows-2">
             {socials.map((social, index) => {
               return (
-                <a
-                  key={index}
-                  href={social.url}
-                  target="_blank"
-                  rel="noreferrer"
-                  aria-label={social.name}
-                  className="text-3xl mx-4 transition duration-200 hover:text-blue-500"
-                >
-                  {social.icon}
-                </a>
+                <div key={index} className="flex items-center justify-evenly px-8 py-2 m-2 mx-2 border-2 border-blue-900 hover:border-blue-500 rounded-xl text-gray-400 hover:text-slate-50 hover:cursor-pointer ">
+                  <span>{social.name}</span>
+                  <a
+                    key={index}
+                    href={social.url}
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label={social.name}
+                    className="mx-4 text-3xl transition duration-200"
+                  >
+                    {social.icon}
+                  </a>
+                </div>
               );
             })}
           </div>
