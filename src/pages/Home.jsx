52a9a3730f66f66547bfa7d9ef06447057a90f65
Santhosh mani <pitakasanthosh@gmail.com>
2024-11-04 17:17:15 +0530

    Modify Home page Buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,13 +14,13 @@ const Home = () => {
       >
         <SpaceContainer />
         <CursorFollowingGraph />
-        <div className="m-4 mx-auto leading-relaxed text-left sm:text-left md:text-2xl sm:w-2/3 w-10/12 px-6 z-10">
-          <h2 className="text-4xl font-bold text-left  sm:text-5xl lg:text-6xl contrast-200 py-8">
+        <div className="m-4 mx-auto leading-relaxed tracking-tight text-left sm:text-left md:text-2xl sm:w-2/3 w-10/12 px-6 z-10">
+          <h2 className="text-4xl text-left  sm:text-5xl lg:text-6xl contrast-200 py-8 tracking-tighter leading-relaxed">
             Hi, I&apos;m Santhosh
           </h2>
           I love building web applications, tinkering with web security, Machine
           Learning &amp; contributing to Open Source Software.
-          <h3 className="text-2xl py-6 text-left sm:text-3xl lg:text-4xl">
+          <h3 className="text-2xl py-4 text-left sm:text-3xl lg:text-4xl lg:pt-12 tracking-tight font-normal">
             Connect With Me
           </h3>
           <div className="flex flex-row space-x-4">
@@ -29,7 +29,7 @@ const Home = () => {
               target="_blank"
               rel="noreferrer"
               aria-label={"github"}
-              className="flex items-center transition duration-300 justify-evenly lg:text-xl  border p-2 rounded-xl cursor-pointer"
+              className="flex items-center transition duration-500 justify-evenly lg:text-xl py-2 px-6 rounded-xl cursor-pointer border-2 text-slate-100 border-slate-400 hover:bg-black hover:text-white hover:border-white"
             >
               <span className="px-1">
                 <FaGithub />
@@ -42,7 +42,7 @@ const Home = () => {
               target="_blank"
               rel="noreferrer"
               aria-label={"mail"}
-              className="flex items-center transition duration-300 justify-evenly lg:text-xl  border p-2 rounded-xl hover:text-white cursor-pointer"
+              className="flex items-center transition duration-500 justify-evenly lg:text-xl py-2 lg:py-3 px-8 rounded-xl cursor-pointer border-2  text-slate-100 border-slate-400 hover:bg-black hover:text-white hover:border-white"
             >
               <span className="px-1">
                 <SiGmail />
