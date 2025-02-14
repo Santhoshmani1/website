00136a3aa2248b5d70b395e821a2cc3fc7a0214d
Santhosh mani <pitakasanthosh@gmail.com>
2025-02-14 08:16:11 +0530

    chore: Remove unused Skills and Resume components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
-import Resume from "./pages/Resume";
 import Post from "./components/blog/Post.jsx";
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} id="1" />
         <Route path="/blog" element={<Blog />} id="2" />
-        <Route path="/resume" element={<Resume />} id="3" />
         <Route path="/blog/:id" element={<Post />} />
       </Routes>
     </>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import Header from "./Header";
-import Skills from "./Skills";
 import Projects from "./Projects";
 import Footer from "./Footer";
 import SpaceContainer from "./SpaceContainer";
 
-export { Header, Skills, Projects, Footer, SpaceContainer };
+export { Header, Projects, Footer, SpaceContainer };
