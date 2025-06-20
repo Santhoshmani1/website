512dd2236d2cf5524a1a315a5c301fc5176a69d3
santhoshmani1 <pitakasanthosh@gmail.com>
2025-06-20 15:08:58 +0530

    lint: configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Open sans", "Inter", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.{html,js,jsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Open sans", "Inter", "sans-serif"],
+			},
+		},
+	},
+	plugins: [require("@nauverse/tailwind-dot-grid-backgrounds")],
 };
--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,25 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "refresh-on-css-change",
-      handleHotUpdate({ file, server }) {
-        if (file.endsWith(".css")) {
-          server.ws.send({
-            type: "full-reload",
-            path: "*",
-          });
-        }
-      },
-    },
-  ],
+	plugins: [
+		react(),
+		{
+			name: "refresh-on-css-change",
+			handleHotUpdate({ file, server }) {
+				if (file.endsWith(".css")) {
+					server.ws.send({
+						type: "full-reload",
+						path: "*",
+					});
+				}
+			},
+		},
+	],
+	server: {
+		watch: {
+			usePolling: true,
+			interval: 1000,
+			ignored: ["!**/src/**/*.{js,ts,jsx,tsx}"],
+		},
+	},
 });
