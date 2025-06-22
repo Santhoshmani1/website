128daee09cfa4de03f5e06104d04fe8f3b5ec6cf
Santhosh mani <pitakasanthosh@gmail.com>
2025-06-22 06:26:17 +0530

    style: fix footer text alignment to be centred

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,10 +2,10 @@ const Footer = () => {
 	return (
 		<>
 			<footer>
-				<div className="flex flex-col items-center p-8 leading-relaxed tracking-tight border-t text-slate-400 bg-slate-950 footer-wrapper justify-evenly border-t-slate-500">
-					<div className="py-2 copyright-info">
+				<div className="flex flex-col items-center p-8 font-light leading-relaxed tracking-tight border-t text-slate-400 bg-slate-950 footer-wrapper justify-evenly border-t-slate-500">
+					<div className="flex flex-col py-2 leading-relaxed tracking-tight text-center md:items-center md:flex-row">
 						<span>&copy; {new Date().getFullYear()} Santhosh Mani. </span>
-						<span className="pl-5">All rights reserved.</span>
+						<span className="py-2 pl-5">All rights reserved.</span>
 					</div>
 					<div>
 						<span>
